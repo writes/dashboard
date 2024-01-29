a71acff83771a58d1c69ea6a535355575220507d
Jonathon Thompson <jonathonhthompson@gmail.com>
2024-01-29 13:32:28 -0800

    initial commit with spring initializer

---
+++ DashboardApp/src/test/java/com/jonathonthompson/DashboardApp/DashboardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathonthompson.DashboardApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
